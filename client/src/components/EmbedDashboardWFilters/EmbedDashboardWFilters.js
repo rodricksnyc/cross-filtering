5be926a43fdb9030ab9ba37172e7d4529b9463f0
Joseph Vega <javega175@gmail.com>
2024-05-12 21:26:23 -0700

    added dynamic dashboard;

--- client/src/components/EmbedDashboardWFilters/EmbedDashboardWFilters.js
+++ client/src/components/EmbedDashboardWFilters/EmbedDashboardWFilters.js
@@ -1,4 +1,3 @@
-
 import React, { useCallback, useEffect } from "react";
 import styled from "styled-components";
 import { LookerEmbedSDK } from "@looker/embed-sdk";
@@ -14,9 +13,11 @@ import {
   useExpressionState,
 } from "@looker/filter-components";
 
+let dashboard = [];
+
 const EmbedDashboardWFilters = () => {
   const [loading, setLoading] = React.useState(true);
-  const [dashboard, setDashboard] = React.useState();
+  // const [dashboard, setDashboard] = React.useState();
 
   // State for all the available filters for the embedded dashboard
   const [dashboardFilters, setDashboardFilters] = React.useState();
@@ -27,12 +28,8 @@ const EmbedDashboardWFilters = () => {
   // Looker API call using the API SDK to get all the available filters for the embedded dashboard
   useEffect(() => {
     const initialize = async () => {
-      const filters = await sdk.ok(
-        sdk.dashboard(
-          "data_block_acs_bigquery::acs_census_overview",
-          "dashboard_filters"
-        )
-      );
+      const filters = await sdk.ok(sdk.dashboard(925, "dashboard_filters"));
+      console.log(filters["dashboard_filters"], "filters");
       setDashboardFilters(filters["dashboard_filters"]);
     };
     initialize();
@@ -40,19 +37,21 @@ const EmbedDashboardWFilters = () => {
 
   // Set the new selected filter values in state, when selected using the components outside the dashboard
   const handleFilterChange = (newFilterValue, filterName) => {
-    // Using the dashboard state, we are sending a message to the iframe to update the filters with the new values
-    dashboard.send("dashboard:filters:update", {
-      filters: {
-        [filterName]: newFilterValue,
-      },
+    dashboard.forEach((dash) => {
+      // // Using the dashboard state, we are sending a message to the iframe to update the filters with the new values
+      dash.send("dashboard:filters:update", {
+        filters: {
+          [filterName]: newFilterValue,
+        },
+      });
+      // The "dashboard:run" message has to be sent for the filter change to take effect
+      dash.send("dashboard:run");
     });
-    // The "dashboard:run" message has to be sent for the filter change to take effect
-    dashboard.send("dashboard:run");
   };
 
   // Set the state of the dashboard so we can update filters and run
   const handleDashboardLoaded = (dashboard) => {
-    setDashboard(dashboard);
+    // setDashboard(dashboard);
     setLoading(false);
   };
 
@@ -61,97 +60,70 @@ const EmbedDashboardWFilters = () => {
    See App.js for reference
   */
 
-  const makeDashboard = useCallback((el) => {
-    if (!el) {
-      return;
-    }
-    el.innerHTML = "";
-    /*
+  const makeDashboard = useCallback(
+    (id) => (el) => {
+      if (!el) {
+        return;
+      }
+
+      el.innerHTML = "";
+      /*
       Step 2 Create your dashboard (or other piece of embedded content) through a simple set of chained methods
     */
-    LookerEmbedSDK.createDashboardWithId(
-      "data_block_acs_bigquery::acs_census_overview"
-    )
-      // Adds the iframe to the DOM as a child of a specific element
-      .appendTo(el)
-      // Hides the filters in the embedded dashboard. Custom themes must be enabled on the Looker instance.
-      //.withParams({_theme: "{\"show_filters_bar\":false}"})
-      // Performs the call to the auth service to get the iframe's src='' url, places it in the iframe and the client performs the request to Looker
-      .build()
-      // Establishes event communication between the iframe and parent page
-      .connect()
-      .then(handleDashboardLoaded)
-      // Catches various errors which can occur in the process (note: does not catch 404 on content)
-      .catch((error) => {
-        console.error("An unexpected error occurred", error);
-      });
-  }, []);
-
-
-  const makeDashboard2 = useCallback((el) => {
-    if (!el) {
-      return;
-    }
-    el.innerHTML = "";
-    /*
-      Step 2b Create your dashboard (or other piece of embedded content) through a simple set of chained methods
-    */
-    LookerEmbedSDK.createDashboardWithId(921)
-      // adds the iframe to the DOM as a child of a specific element
-      .appendTo(el)
-      // this instructs the SDK to point to the /dashboards-next/ version
-      .withNext()
-
+      LookerEmbedSDK.createDashboardWithId(id)
+        // Adds the iframe to the DOM as a child of a specific element
+        .appendTo(el)
+        // Hides the filters in the embedded dashboard. Custom themes must be enabled on the Looker instance.
         //.withParams({_theme: "{\"show_filters_bar\":false}"})
-      // this line performs the call to the auth service to get the iframe's src='' url, places it in the iframe and the client performs the request to Looker
-      .build()
-      // this establishes event communication between the iframe and parent page
-      .connect()
-      // catch various errors which can occur in the process (note: does not catch 404 on content)
-      .catch((error) => {
-        console.error("An unexpected error occurred", error);
-      });
-  }, []);
+        // Performs the call to the auth service to get the iframe's src='' url, places it in the iframe and the client performs the request to Looker
+        .build()
+        // Establishes event communication between the iframe and parent page
+        .connect()
+        .then((response) => {
+          dashboard.push(response);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error(error.message);
+        });
+    },
+    []
+  );
 
   return (
+    <div className={"embed-dashboard-main"} style={{ padding: "20px" }}>
+      <PageTitle
+        text={"Two Dashboards with Filters"}
+        style={{ marginBottom: "50px" }}
+      />
+      <LoadingSpinner loading={loading} />
+      <ComponentsProvider resources={i18nResources}>
+        <Space m="u3" align="end" width="auto">
+          {dashboardFilters?.map((filter) => {
+            return (
+              <DashFilters
+                filter={filter}
+                expression={filterValues[filter.name]}
+                onChange={(event) => handleFilterChange(event, filter.name)}
+                key={filter.id}
+              />
+            );
+          })}
+        </Space>
+      </ComponentsProvider>
+      {/* Step 0) we have a simple container, which performs a callback to our makeDashboard function */}
+      <br />
 
-      <div className={"embed-dashboard-main"} style={{padding:"20px"}}>
-        <PageTitle text={"Two Dashboards with Filters"}
-        style={{marginBottom:"50px"}} />
-        <LoadingSpinner loading={loading} />
-        <ComponentsProvider resources={i18nResources}>
-          <Space m="u3" align="end" width="auto">
-            {dashboardFilters?.map((filter) => {
-              return (
-                <DashFilters
-                  filter={filter}
-                  expression={filterValues[filter.name]}
-                  onChange={(event) => handleFilterChange(event, filter.name)}
-                  key={filter.id}
-                />
-              );
-            })}
-          </Space>
-        </ComponentsProvider>
-        {/* Step 0) we have a simple container, which performs a callback to our makeDashboard function */}
-        <br/>
-
-
-        <p>Dashboard 1</p>
-        <Dashboard ref={makeDashboard}></Dashboard>
-        <br/>
-
-        <br/>
-
-        <br/>
-
-        <br/>
-
-        <p>Dashboard 2</p>
-        <Dashboard2 ref={makeDashboard2}></Dashboard2>
-
-      </div>
-
+      {[925, 927].map((id, index) => (
+        <div key={index}>
+          <p>Dashboard {id}</p>
+          <Dashboard ref={makeDashboard(id)}></Dashboard>
+          <br />
+          <br />
+          <br />
+        </div>
+      ))}
+    </div>
   );
 };
 
@@ -159,8 +131,8 @@ const EmbedDashboardWFilters = () => {
 const Dashboard = styled.div`
   width: 100%;
   height: 40vh;
-  bargin-bottom:200px;
-  border:1px solid purple;
+  bargin-bottom: 200px;
+  border: 1px solid purple;
   & > iframe {
     width: 100%;
     height: 100%;
@@ -168,9 +140,9 @@ const Dashboard = styled.div`
 `;
 
 const Dashboard2 = styled.div`
-width: 100%;
-height: 40vh;
-  border:1px solid purple;
+  width: 100%;
+  height: 40vh;
+  border: 1px solid purple;
   & > iframe {
     width: 100%;
     height: 100%;
@@ -205,7 +177,7 @@ export const DashFilters = ({ filter, expression, onChange }) => {
   `;
 
   return (
-<>
+    <>
       <FilterLabel>{filter.name}</FilterLabel>
       <Filter
         name={filter.name}
@@ -214,6 +186,6 @@ export const DashFilters = ({ filter, expression, onChange }) => {
         {...suggestableProps}
         {...stateProps}
       />
-</>
+    </>
   );
 };
